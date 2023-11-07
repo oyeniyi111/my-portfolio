a92a822d1c7679b9c81606c86ab0c1186d320b85
Oyeniyi Tolulope John <140219335+oyeniyi111@users.noreply.github.com>
2023-11-07 20:57:30 +0100

    Update portfolio.js

--- js/portfolio.js
+++ js/portfolio.js
@@ -30,21 +30,6 @@ window.addEventListener("scroll", () => {
    }) 
 
 
-// accodian
-var acc = document.getElementsByClassName("accordion");
-var i;
-
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var panel = this.nextElementSibling;
-    if (panel.style.display === "block") {
-      panel.style.display = "none";
-    } else {
-      panel.style.display = "block";
-    }
-  });
-}
 
 
 // skills-tab
